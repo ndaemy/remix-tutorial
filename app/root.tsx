77d78cb6d7deaddd7e1d99597bbe8bb16942b57d
ndaemy <yuyaebean@gmail.com>
2021-11-29 13:26:50 +0900

    🎉 Initial commit from create-remix (jokes app init)

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,13 @@
-import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch } from "remix";
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch
+} from "remix";
 import type { LinksFunction } from "remix";
 
 import globalStylesUrl from "~/styles/global.css";
@@ -11,8 +20,8 @@ export let links: LinksFunction = () => {
     {
       rel: "stylesheet",
       href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)",
-    },
+      media: "(prefers-color-scheme: dark)"
+    }
   ];
 };
 
@@ -38,7 +47,10 @@ export function ErrorBoundary({ error }: { error: Error }) {
           <h1>There was an error</h1>
           <p>{error.message}</p>
           <hr />
-          <p>Hey, developer, you should replace this with what you want your users to see.</p>
+          <p>
+            Hey, developer, you should replace this with what you want your
+            users to see.
+          </p>
         </div>
       </Layout>
     </Document>
@@ -52,10 +64,17 @@ export function CatchBoundary() {
   let message;
   switch (caught.status) {
     case 401:
-      message = <p>Oops! Looks like you tried to visit a page that you do not have access to.</p>;
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
       break;
     case 404:
-      message = <p>Oops! Looks like you tried to visit a page that does not exist.</p>;
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
       break;
 
     default:
@@ -74,7 +93,13 @@ export function CatchBoundary() {
   );
 }
 
-function Document({ children, title }: { children: React.ReactNode; title?: string }) {
+function Document({
+  children,
+  title
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
@@ -106,9 +131,6 @@ function Layout({ children }: { children: React.ReactNode }) {
             <ul>
               <li>
                 <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/posts">Posts</Link>
               </li>
               <li>
                 <a href="https://remix.run/docs">Remix Docs</a>

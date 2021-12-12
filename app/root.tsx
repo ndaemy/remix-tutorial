d20f4e3d5ac9d2ee3bf49b078dc0f9ca952789d0
ndaemy <yuyaebean@gmail.com>
2021-12-12 21:57:33 +0900

    🐛 Fix Scripts component position

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,7 @@ export const meta: MetaFunction = () => {
     "twitter:description": description,
   };
 };
+
 function Document({ children, title = "Remix, So great, it's funny!" }: { children: React.ReactNode; title?: string }) {
   return (
     <html lang="en">
@@ -45,17 +46,9 @@ function Document({ children, title = "Remix, So great, it's funny!" }: { childr
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
+    <Document>
+      <Outlet />
+    </Document>
   );
 }
 

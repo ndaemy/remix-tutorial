d2e2ae3e99ce8342e22a43172ac9eaa34e27dcd6
ndaemy <yuyaebean@gmail.com>
2021-12-28 21:30:03 +0900

    🚸 Optimistic UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,9 @@
 import type { ActionFunction, LoaderFunction, MetaFunction } from "remix";
-import { Form, Link, redirect, useCatch, useLoaderData, useParams } from "remix";
+import { redirect, useCatch, useLoaderData, useParams } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
+import { JokeDisplay } from "~/components/joke";
 
 export const meta: MetaFunction = ({ data }: { data?: LoaderData }) => {
   if (!data) {
@@ -50,21 +51,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 export default function JokeRoute() {
   const data = useLoaderData();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      {data.isOwner && (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      )}
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
